c116ac96ffb8a5b430bd9bfa62806f0fa7fbf436
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-02-11 01:31:10 -0600

    basic contact + new nav?

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,9 +1,28 @@
 import React, {Component} from "react";
+import './contact.css';
 
 export default class Contact extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <div className={'half'}>
+          <ul>
+            <li>Phone</li>
+            <li>Email</li>
+            <li>GitHub</li>
+            <li>Linkedin</li>
+          </ul>
+        </div>
+        <div className={'half'}>
+          <form>
+            <div><input type={'text'} placeholder={'Your Name'} aria-placeholder={'Your Name'} required aria-required/></div>
+            <div><input type={'text'} placeholder={'Your Email'} aria-placeholder={'Your Email'}
+                   pattern={"\A(?=[a-z0-9@.!#$%&'*+/=?^_‘{|}~-]{6,254}\z)(?=[a-z0-9.!#$%&'*+/=?^_‘{|}~-]{1,64}@)[a-z0-9!#$%&'*+/=?^_‘{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_‘{|}~-]+)*@ (?:(?=[a-z0-9-]{1,63}\.)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?=[a-z0-9-]{1,63}\z)[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\z"} required aria-required/></div>
+            <div><textarea type={'text'} placeholder={'Your Message'} aria-placeholder={'Your Message'} required aria-required/></div>
+            <div><button>Send</button></div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -11,6 +11,21 @@ export default class Menu extends Component {
           <li data-menuanchor="projects"><a href="#projects">Projects</a></li>
           <li data-menuanchor="blog"><a href="#blog">Blog</a></li>
           <li data-menuanchor="contact"><a href="#contact">Contact</a></li>
+          <li>
+            <input id="burgerTick" type="checkbox"/>
+            <label htmlFor="burgerTick" className={'burger'}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+            <nav id={'fullscreenNav'}>
+              <ul>
+                <li><a href="#">Link #1</a></li>
+                <li><a href="#">Link #2</a></li>
+                <li><a href="#">Link #3</a></li>
+              </ul>
+            </nav>
+          </li>
         </ul>
       </div>
     )
