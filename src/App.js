b0b48ed2f2bc09b71092a69f6627dc57b1fd3495
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-02-13 13:16:46 -0600

    pass sections as props to menu, use feather icon lib

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
+import * as Icon from 'react-feather';
 import 'typeface-lato';
 import './App.css';
 import Menu from './components/menu';
@@ -14,6 +15,7 @@ import Contact from './components/contact';
 // https://fonts.google.com/specimen/Lato?selection.family=Lato
 // https://gist.github.com/CodeMyUI/5376e109921ab4609c1857cf8010c4b8
 // https://stackoverflow.com/questions/20751937/transition-background-color-via-slide-up-animation
+// https://feathericons.com/
 
 function App() {
 
@@ -21,46 +23,64 @@ function App() {
     palette: "dark",
   });
 
-  const [anchors, setAnchors] = useState({
-    links: ['fp-intro', 'fp-about', 'fp-resume', 'fp-projects', 'fp-blog', 'fp-contact']
-  });
-
+  const sections = [
+    {
+      anchor:'fp-intro',
+      title: 'Home',
+      icon: (<Icon.Home/>),
+      section: (<Introduction/>)
+    },
+    {
+      anchor: 'fp-about',
+      title: 'About',
+      icon: (<Icon.User/>),
+      section: (<About/>)
+    },
+    {
+      anchor: 'fp-resume',
+      title: 'Resume',
+      icon: (<Icon.FileText/>),
+      section: (<Resume/>)
+    },
+    {
+      anchor: 'fp-projects',
+      title: 'Projects',
+      icon: (<Icon.Package/>),
+      section: (<Projects/>)
+    },
+    {
+      anchor: 'fp-blog',
+      title: 'Blog',
+      icon: (<Icon.Book/>),
+      section: (<Blog/>)
+    },
+    {
+      anchor: 'fp-contact',
+      title: 'Contact',
+      icon: (<Icon.Info/>),
+      section: (<Contact/>)
+    }
+  ];
 
   return (
     <div className="App" theme={theme.palette}>
-      <Menu setTheme={(palette) => setTheme({palette})} setAnchors={(links) => setAnchors({links})} />
-      <div id={'main'}>
-        <ReactFullpage
-          licenseKey={'OPEN-SOURCE-GPLV3-LICENSE'}
-          css3={true}
-          anchors={anchors.links}
-          menu={'#menu'}
-          render={({state, fullpageApi}) => {
-            return (
-              <ReactFullpage.Wrapper>
-                <div className={'section'}>
-                  <Introduction />
+      <Menu setTheme={(palette) => setTheme({palette})} sections={sections} />
+      <ReactFullpage
+        licenseKey={'OPEN-SOURCE-GPLV3-LICENSE'}
+        css3={true}
+        menu={'#menu'}
+        render={({state, fullpageApi}) => {
+          return (
+            <ReactFullpage.Wrapper>
+              {sections.map((element, index) => (
+                <div className={'section'} data-anchor={element.anchor} key={index}>
+                  {element.section}
                 </div>
-                <div className={'section'}>
-                  <About />
-                </div>
-                <div className={'section'}>
-                  <Resume />
-                </div>
-                <div className={'section'}>
-                  <Projects />
-                </div>
-                <div className={'section'}>
-                  <Blog />
-                </div>
-                <div className={'section'}>
-                  <Contact />
-                </div>
-              </ReactFullpage.Wrapper>
-            );
-          }}
-        />
-      </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     </div>
   )
 }
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,116 +1,12 @@
 import React, {Component} from "react";
+import * as Icon from 'react-feather';
 import './menu.scss';
-import home from '../imgs/home.svg'
-import about from '../imgs/user.svg'
-import resume from '../imgs/file-text.svg'
-import project from '../imgs/package.svg'
-import blog from '../imgs/book.svg'
-import contact from '../imgs/info.svg'
-import light from '../imgs/sun.svg'
-import dark from '../imgs/moon.svg'
-
-const links = [
-  {
-    anchor:'fp-intro',
-    title: 'Home',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-           stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-           className="feather feather-home">
-        <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-        <polyline points="9 22 9 12 15 12 15 22"/>
-        <image src={home} xlinkHref={''} />
-      </svg>
-    )
-  },
-  {
-    anchor: 'fp-about',
-    title: 'About',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-               stroke="black" strokeWidth="2" strokeLinecap="butt" strokeLinejoin="miter"
-               className="feather feather-user">
-        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/>
-        <circle cx="12" cy="7" r="4"/>
-        <image src={about} xlinkHref={''} />
-      </svg>
-    )
-  },
-  {
-    anchor: 'fp-resume',
-    title: 'Resume',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-           stroke="currentColor" strokeWidth="2" strokeLinecap="butt" strokeLinejoin="miter"
-           className="feather feather-file-text">
-        <script xmlns=""/>
-        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
-        <polyline points="14 2 14 8 20 8"/>
-        <line x1="16" y1="13" x2="8" y2="13"/>
-        <line x1="16" y1="17" x2="8" y2="17"/>
-        <polyline points="10 9 9 9 8 9"/>
-        <image src={resume} xlinkHref={''} />
-      </svg>
-    )
-  },
-  {
-    anchor: 'fp-projects',
-    title: 'Projects',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-           stroke="black" strokeWidth="2" strokeLinecap="butt" strokeLinejoin="miter"
-           className="feather feather-package">
-        <script xmlns=""/>
-        <line x1="16.5" y1="9.4" x2="7.5" y2="4.21"/>
-        <path
-          d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
-        <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
-        <line x1="12" y1="22.08" x2="12" y2="12"/>
-        <image src={project} xlinkHref={''} />
-      </svg>
-    )
-  },
-  {
-    anchor: 'fp-blog',
-    title: 'Blog',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-           stroke="black" strokeWidth="2" strokeLinecap="butt" strokeLinejoin="miter"
-           className="feather feather-book">
-        <script xmlns=""/>
-        <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/>
-        <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
-        <image src={blog} xlinkHref={''} />
-      </svg>
-    )
-  },
-  {
-    anchor: 'fp-contact',
-    title: 'Contact',
-    svg: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-           stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-           className="feather feather-info">
-        <script xmlns=""/>
-        <circle cx="12" cy="12" r="10"/>
-        <line x1="12" y1="16" x2="12" y2="12"/>
-        <line x1="12" y1="8" x2="12.01" y2="8"/>
-        <image src={contact} xlinkHref={''}/>
-      </svg>
-    )
-  }
-];
 
 export default class Menu extends Component {
 
   constructor(props) {
     super(props);
     this.props = props;
-    let anchors = [];
-    links.forEach((element) => {
-      anchors.push(element.anchor);
-    });
-    this.props.setAnchors(anchors);
   }
 
   render() {
@@ -119,37 +15,17 @@ export default class Menu extends Component {
         <div id={'menu-desktop'}>
           <div id={'themeToggle'}>
             <div onClick={() => this.props.setTheme('light')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-                   stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
-                   className="feather feather-moon">
-                <script xmlns=""/>
-                <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
-                <image src={dark} xlinkHref={''} />
-              </svg>
+              <Icon.Moon strokeWidth={'1.5'}/>
             </div>
             <div onClick={() => this.props.setTheme('dark')}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24" fill="none"
-                   stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
-                   className="feather feather-sun">
-                <script xmlns=""/>
-                <circle cx="12" cy="12" r="5"/>
-                <line x1="12" y1="1" x2="12" y2="3"/>
-                <line x1="12" y1="21" x2="12" y2="23"/>
-                <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"/>
-                <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"/>
-                <line x1="1" y1="12" x2="3" y2="12"/>
-                <line x1="21" y1="12" x2="23" y2="12"/>
-                <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"/>
-                <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"/>
-                <image src={light} xlinkHref={''} />
-              </svg>
+              <Icon.Sun strokeWidth={'1.5'}/>
             </div>
           </div>
           <ul>
-            {links.map((element, index) => (
+            {this.props.sections.map((element, index) => (
               <li data-menuanchor={element.anchor} key={index}>
                 <a href={'#' + element.anchor}>
-                  {element.svg}
+                  {element.icon}
                 </a>
                 <div id={'title'} key={index}>{element.title}</div>
               </li>
@@ -165,7 +41,7 @@ export default class Menu extends Component {
           </label>
           <nav>
             <ul>
-              {links.map((element, index) => (
+              {this.props.sections.map((element, index) => (
                 <li data-menuanchor={element.anchor} key={index}>
                   <a href={'#' + element.anchor}>{element.title}</a>
                 </li>
