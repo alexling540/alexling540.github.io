c8b642721b925d2cb2624c5584f1cd2b7b6eabb6
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-05-08 17:26:36 -0500

    Swiper test

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {BrowserView, MobileView} from "react-device-detect";
+import Swiper from 'react-id-swiper';
 import {Card, CardColumns} from "react-bootstrap";
 
 export default class Projects extends Component {
@@ -8,29 +10,61 @@ export default class Projects extends Component {
       {
         title: 'Ag\'s Guide to Foods And Brews',
         image: '',
-        desc: '',
-        item: [],
+        desc: 'Vue, Vuetify, Axios',
         link: 'https://pages.github.tamu.edu/jarrett/Group7FinalProject/#/',
         repo: 'https://github.tamu.edu/jarrett/Group7FinalProject'
+      },
+      {
+        title: 'Test',
+        image: '',
+        desc: '',
+        repo: 'https://github.tamu.edu/peterluong/group14databaseproject'
       }
     ];
+
+    const params = {
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+      }
+    };
 
     return(
       <React.Fragment>
         <h1>Projects</h1>
-        <CardColumns>
-        {cards.map((element, i) => (
-          <Card key={i}>
-            <Card.Img variant="top" src={element.image} />
-            <Card.Body>
-              <Card.Title>{element.title}</Card.Title>
-              <Card.Text>{element.desc}</Card.Text>
-              <Card.Link href={element.link}>Live</Card.Link>
-              <Card.Link href={element.repo}>Github</Card.Link>
-            </Card.Body>
-          </Card>
-        ))}
-        </CardColumns>
+        <BrowserView renderWithFragment>
+          <CardColumns>
+            {cards.map((element, i) => (
+              <Card key={i}>
+                <Card.Img variant="top" src={element.image} />
+                <Card.Body>
+                  <Card.Title>{element.title}</Card.Title>
+                  <Card.Text>{element.desc}</Card.Text>
+                  <Card.Link href={element.link}>Live</Card.Link>
+                  <Card.Link href={element.repo}>Github</Card.Link>
+                </Card.Body>
+              </Card>
+            ))}
+          </CardColumns>
+        </BrowserView>
+        <MobileView>
+          <Swiper {...params}>
+            {cards.slice(4).map((element, i) => (
+              <Card key={i}>
+                <Card.Img variant="top" src={element.image} />
+                <Card.Body>
+                  <Card.Title>{element.title}</Card.Title>
+                  <Card.Text>{element.desc}</Card.Text>
+                  <Card.Link href={element.link}>Live</Card.Link>
+                  <Card.Link href={element.repo}>Github</Card.Link>
+                </Card.Body>
+              </Card>
+            ))}
+          </Swiper>
+        </MobileView>
       </React.Fragment>
     );
   }
